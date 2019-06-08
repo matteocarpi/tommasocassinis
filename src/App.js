8d8859ec4b811a73f3083ab3f6bc3743e9b7dc8e
Matteo Carpi <matteocarpi94@gmail.com>
2019-06-08 13:49:38 +0200

    almost finished video component and loop for rendering it in APP

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ import {
   Route
 } from 'react-router-dom';
 
+import Videos from './data/video.json';
 import Video from './components/elements/video';
 import Header from './components/elements/headerComponent/header';
 
-
+const Child = ({ match }) => (
+  <div>
+    <h3>ID: {match.params.id}</h3>
+  </div>
+)
 
 function App() {
   return (
@@ -19,6 +24,24 @@ function App() {
 
         <Header />
 
+        {/* {Videos.map((video, index) => {
+            return <Video title={video.title} />
+          }
+        )} */}
+
+        {Videos.map((video, index) => {
+            let permalink = '/video/' + video.title.replace(/\s+/g, '-').toLowerCase();
+            return <Route exact path={permalink} render={() => <Video
+              title={video.title}
+              link={video.link}
+              copyright={video.copyright}
+              description={video.description}
+              crew={video.crew}
+              specialThanks={video.specialThanks}
+              otherInfo={video.otherInfo}
+              partners={video.partners}
+            />} />
+          })}
     </div>
     </Router>
   );
---
+++ src/components/elements/video.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import ReactPlayer from 'react-player';
+
+import '../../assets/styles/video.scss';
+function Video (video) {
+  return (
+    <div className="video-page">
+
+      <ReactPlayer className="embedded-video" url={video.link} />
+
+      <div className="video-info">
+
+        <div className="video-title">
+          <h2>{video.title}</h2>
+          <span className="copyright">©{video.copyright}</span>
+        </div>
+
+        <div className="video-description">
+          <p>{video.description}</p>
+        </div>
+
+        <div className="crew">
+          <h3>CREW</h3>
+
+          {video.crew.map((crew, index) => {
+              return (
+                  <p className="crew-role"><strong>{crew.role}</strong>:{crew.name}</p>
+              )
+          })}
+        </div>
+
+          <div classNames="partners">
+
+            {video.partners.map((partner, index) => {
+                return (
+                    <img src={partner.logo}></img>
+                )
+            })}
+          </div>
+
+
+      </div>
+    </div>
+  );
+}
+
+
+export default Video;
