475ce476caa863cc9565322bb9a587c5f4d06f05
Matteo Carpi <matteocarpi94@gmail.com>
2019-06-09 14:15:58 +0200

    contact page

---
+++ src/components/pages/contactPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import '../../assets/styles/contactPage.scss';
+
+class ContactPage extends React.Component {
+  render() {
+    return (
+      <div className="contact-page page-wrap">
+        <main className="tommy">
+        </main>
+
+        <div className="side-column" >
+          <img className="partner-logo" src="/assets/img/partners/Logo-Controluce-Nero.png" alt=""/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ContactPage;
