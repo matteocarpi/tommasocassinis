bac1e9c07e49faaf40e5eee28319877663ee8c28
Matteo Carpi <matteocarpi94@gmail.com>
2019-06-10 12:32:57 +0200

    Updates

---
+++ precache-manifest.357a31e1a3b06c6d47889e9d1ccd3a55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e67a86d469852cf6943502ff9accf4",
+    "url": "/tommasocassinis/index.html"
+  },
+  {
+    "revision": "e115a748d64b9f2cd954",
+    "url": "/tommasocassinis/static/css/main.7ffec42e.chunk.css"
+  },
+  {
+    "revision": "e5233237404f44b5d062",
+    "url": "/tommasocassinis/static/js/2.43209e3a.chunk.js"
+  },
+  {
+    "revision": "e115a748d64b9f2cd954",
+    "url": "/tommasocassinis/static/js/main.2cb515d0.chunk.js"
+  },
+  {
+    "revision": "9f316b25f7314af6a706",
+    "url": "/tommasocassinis/static/js/runtime~main.2124000d.js"
+  },
+  {
+    "revision": "d321fa78bb7190a8ca7e14213ef63203",
+    "url": "/tommasocassinis/static/media/Aileron-Regular.d321fa78.otf"
+  },
+  {
+    "revision": "a6509aa3161205ebe01c0f7e2f209b44",
+    "url": "/tommasocassinis/static/media/LeagueSpartan-Bold.a6509aa3.otf"
+  }
+]);
