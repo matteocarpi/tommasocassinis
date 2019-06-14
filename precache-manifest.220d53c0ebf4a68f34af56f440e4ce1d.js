398462f0cb88885d7c4a1f40e29178eb4a1bc92b
Matteo Carpi <matteocarpi94@gmail.com>
2019-06-14 14:39:38 +0200

    Updates

---
+++ precache-manifest.220d53c0ebf4a68f34af56f440e4ce1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce43ea79357cb368cbc8085e77587c9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1191d280e7c9abf3ff3",
+    "url": "/static/css/main.6843544f.chunk.css"
+  },
+  {
+    "revision": "e5233237404f44b5d062",
+    "url": "/static/js/2.43209e3a.chunk.js"
+  },
+  {
+    "revision": "a1191d280e7c9abf3ff3",
+    "url": "/static/js/main.d740324d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d321fa78bb7190a8ca7e14213ef63203",
+    "url": "/static/media/Aileron-Regular.d321fa78.otf"
+  },
+  {
+    "revision": "a6509aa3161205ebe01c0f7e2f209b44",
+    "url": "/static/media/LeagueSpartan-Bold.a6509aa3.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tommasocassinis/precache-manifest.b2b236af74d3fa712d92d02a131184e3.js"
+  "/precache-manifest.220d53c0ebf4a68f34af56f440e4ce1d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tommasocassinis/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
