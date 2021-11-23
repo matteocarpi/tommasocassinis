2f2103fe247ca21184b368d0194dceca6dba0187
Matteo Carpi <matteocarpi94@gmail.com>
2021-11-23 12:12:24 +0100

    fix: email

--- src/components/pages/contactPage.js
+++ src/components/pages/contactPage.js
@@ -1,32 +1,53 @@
 import React from "react";
 
-import '../../assets/styles/contactPage.scss';
+import "../../assets/styles/contactPage.scss";
 
 class ContactPage extends React.Component {
   render() {
     return (
       <div className="contact-page page-wrap">
-        <main className="tommy">
-        </main>
+        <main className="tommy"></main>
 
-        <div className="side-column" >
-
-          <img className="partner-logo" src="/assets/img/partners/Logo-Controluce-Nero.png" alt=""/>
+        <div className="side-column">
+          <img
+            className="partner-logo"
+            src="/assets/img/partners/Logo-Controluce-Nero.png"
+            alt=""
+          />
 
           <h2>ROME / ITALY</h2>
 
-          <a href="mailto:tommsocassinis@gmail.com">tommasocassinis@gmail.com</a>
+          <a href="mailto:tommaso.cassinis@gmail.com">
+            tommaso.cassinis@gmail.com
+          </a>
 
           <div className="social">
-            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/tommaso_cassinis/"><i className=" first fab fa-instagram"></i></a>
-            <a target="_blank" rel="noopener noreferrer" href="https://vimeo.com/tommasocassinis"><i className="fab fa-vimeo-v"></i></a>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sommato.sinissac"><i className="fab fa-facebook-f"></i></a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/tommaso_cassinis/"
+            >
+              <i className=" first fab fa-instagram"></i>
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://vimeo.com/tommasocassinis"
+            >
+              <i className="fab fa-vimeo-v"></i>
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/sommato.sinissac"
+            >
+              <i className="fab fa-facebook-f"></i>
+            </a>
             {/* <a href=""><i class="fab fa-twitter"></i></a> */}
           </div>
-
         </div>
       </div>
-    )
+    );
   }
 }
 
